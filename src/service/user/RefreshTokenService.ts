97bc1a0727f61370acbf7a8c75ddbf3c32f8bbd7
RussianCabbageSoup <testylya333@gmail.com>
2026-09-25 00:50:36 +0300

    feature: add hash for token

--- src/service/user/RefreshTokenService.ts
+++ src/service/user/RefreshTokenService.ts
@@ -1,11 +1,12 @@
 import type { Char } from "@prisma/orm-postgres/target/codec-types";
 import { db } from "../../prisma/db";
+import { createHash } from "crypto";
 
 class RefreshTokenService {
     async createToken(userId: Char<36>, token: string) {
         return await db.orm.public.RefreshToken.create({
             userId,
-            token,
+            token: createHash('sha256').update(token).digest('hex'),
             expireAt: new Date(Date.now() + 30 * 24 * 60 * 60 * 1000).toISOString(),
             createdAt: new Date().toISOString()
         });
